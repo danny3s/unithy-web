2e62156fadaf7b9e0d2edd90c30be20efa23c21d
Daniel <dahusa.92@gmail.com>
2018-10-15 15:15:16 -0500

    combinar

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+  var mainChart = new RadialProgressChart('.main', {
+		diameter: 130,
+		series: [
+			{labelStart: '\uF106', value: 3, color: '#f5ea5c'},
+			{labelStart: '\uF101', value: 12, color: '#ff8e69'},
+			{labelStart: '\uF105', value: 15, color: '#f56076'}
+		]}
+);
+
+
+
+var mainChart = new RadialProgressChart('.main2', {
+	diameter: 130,
+	series: [
+		{labelStart: '\uF106', value: 8, color: '#f5ea5c'},
+		{labelStart: '\uF101', value: 10, color: '#ff8e69'},
+		{labelStart: '\uF105', value: 18, color: '#f56076'}
+	]}
+);
+
+
+
+var mainChart = new RadialProgressChart('.main3', {
+	diameter: 130,
+	series: [
+		{labelStart: '\uF106', value: 3, color: '#f5ea5c'},
+		{labelStart: '\uF101', value: 15, color: '#ff8e69'},
+		{labelStart: '\uF105', value: 18, color: '#f56076'}
+	]}
+);
+
+var mainChart = new RadialProgressChart('.main4', {
+	diameter: 130,
+	series: [
+		{labelStart: '\uF106', value: 12, color: '#f5ea5c'},
+		{labelStart: '\uF101', value: 8.5, color: '#ff8e69'},
+		{labelStart: '\uF105', value: 20.5, color: '#f56076'}
+	]}
+);
+
+
+var mainChart = new RadialProgressChart('.main5', {
+	diameter: 130,
+	series: [
+		{labelStart: '\uF106', value: 1.5, color: '#f5ea5c'},
+		{labelStart: '\uF101', value: 1.5, color: '#ff8e69'},
+		{labelStart: '\uF105', value: 3, color: '#f56076'}
+	]}
+);
